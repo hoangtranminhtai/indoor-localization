14224346017d0e82971c550783444eb094b5168d
Hoàng Trần Minh Tài <tai.htm@gmail.com>
2019-03-27 22:24:31 +0700

    update

--- main.go
+++ main.go
@@ -4,31 +4,69 @@ import (
 	"fmt"
 	"html/template"
 	"net/http"
+	"net/url"
+	"strconv"
 )
 
 //func SimpleIndexHandler(w http.ResponseWriter, r *http.Request) {
 //	fmt.Fprintf(w, "Hello %s!", r.URL.Path[1:])
 //}
 
-type Student struct {
-	Id   int
-	Name string //exported field since it begins with a capital letter
+type Position struct {
+	Lat float32
+	Lng float32 //exported field since it begins with a capital letter
+}
+
+type Error struct {
+	Err string //exported field since it begins with a capital letter
 }
 
 func HttpFileHandler(response http.ResponseWriter, request *http.Request) {
-	tmplt := template.New("index.html")       //create a new template with some name
-	tmplt, _ = tmplt.ParseFiles("index.html") //parse some content and generate a template, which is an internal representation
 
-	p := Student{Id: 1, Name: "Aisha"} //define an instance with required field
+	temp := template.New("index.html")      //create a new template with some name
+	temp, _ = temp.ParseFiles("index.html") //parse some content and generate a template, which is an internal representation
 
-	tmplt.Execute(response, p) //merge template ‘t’ with content of ‘p’
+	rawQuery := request.URL.RawQuery
+
+	params, err := url.ParseQuery(rawQuery)
+	if err != nil {
+		http.ServeFile(response, request, "error.html")
+		return
+	}
+
+	_, isHasLat := params["lat"]
+	if !isHasLat {
+		http.ServeFile(response, request, "error.html")
+		return
+	}
+
+	_, isHasLng := params["lng"]
+	if !isHasLng {
+		http.ServeFile(response, request, "error.html")
+		return
+	}
+
+	lat, err := strconv.Atoi(params["lat"][0])
+	if err != nil {
+		http.ServeFile(response, request, "error.html")
+		return
+	}
+
+	lng, err := strconv.Atoi(params["lng"][0])
+	if err != nil {
+		http.ServeFile(response, request, "error.html")
+		return
+	}
+
+	p := Position{float32(lat), float32(lng)} //define an instance with required field
+
+	temp.Execute(response, p) //merge template ‘t’ with content of ‘p’
 }
 
 func main() {
 
-	fmt.Println("Server Starting")
-	//http.HandleFunc("/", SimpleIndexHandler)
-	http.HandleFunc("/index", HttpFileHandler)
+	fmt.Println("Server listening at port 8080...................")
+	http.HandleFunc("/localization", HttpFileHandler)
 
 	http.ListenAndServe(":8080", nil)
 }
